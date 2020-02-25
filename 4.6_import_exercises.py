21508f187894fc53e51784820dcb76439eba2ba2
Ravinder Singh <ravinder79@gmail.com>
2020-02-25 08:54:57 -0600

    4.6 import exercises

--- 4.6_import_exercises.py
+++ 4.6_import_exercises.py
@@ -21,7 +21,7 @@ for i in itertools.product('abc', '123'):
     print(i)
 
 #How many different ways can you combine two of the letters from "abcd"?
-
+import itertools
 for i in itertools.combinations('abcd', 2):
     print(i)
 
@@ -33,17 +33,17 @@ profiles = json.load(open("profiles.json"))
 
 #Total number of users
 users = len(profiles)
-print(users)
+print(f"Total number of users {users}")
  
 #Number of active users
 user = profiles[0]
 
 active_users = len([user for user in profiles if user["isActive"] == True])
-print(active_users)
+print(f" Number of active users: {active_users}")
 
 #Number of inactive users
-active_users = len([user for user in profiles if user["isActive"] == False])
-print(active_users)
+inactive_users = len([user for user in profiles if user["isActive"] == False])
+print(f" Number of inactive users: {inactive_users}")
 
 #Grand total of balances for all users
 balance = [user['balance'] for user in profiles]
@@ -53,23 +53,23 @@ for i in balance:
     i = i[1:]
     i = i.replace(',', '')
     total = total + float(i)
-print(total)
+print(f" Grand total of balances: $ {total}")
 
 
 #Average balance per user
 
 average_balance = total/users
-print(average_balance)
+print(f" Average balance: {average_balance}")
 
 #User with the lowest balance
 low_balance = profiles[-1]['balance']
-print(low_balance)
+
 for i in range(0, len(profiles)):
     if profiles[i]['balance'] < low_balance:
         low_balance = profiles[i]['balance']
         low_user = profiles[i]['name']
 
-print(low_user, low_balance) 
+print(f" User with lowest balance: {low_user}, balance:  {low_balance}") 
 
 #User with the highest balance
 
@@ -79,23 +79,23 @@ for i in range(0, len(profiles)):
     if profiles[i]['balance'] > high_balance:
         high_balance = profiles[i]['balance']
         high_balance_user = profiles[i]['name']
-print(high_balance_user, high_balance)
+print(f"User with highest balance: {high_balance_user}, Balance: {high_balance}")
 
 #Most common favorite fruit
 favoriteFruit = [user['favoriteFruit'] for user in profiles]
-print(favoriteFruit)
+
 fav_fruit_dict = {x: favoriteFruit.count(x) for x in favoriteFruit}
 
 # fav_fruit = max(fav_fruit_dict.keys(), key=(lambda k: fav_fruit_dict[k]))
 # print(fav_fruit)
 most_fav_fruit = max(fav_fruit_dict, key = fav_fruit_dict.get)
-print(most_fav_fruit)
+print(f" Most common fav fruit is: {most_fav_fruit}")
 
 #Least most common favorite fruit
 # least_fav_fruit = min(fav_fruit_dict.keys(), key=(lambda k: fav_fruit_dict[k]))
 # print(least_fav_fruit)
 least_fav_fruit = min(fav_fruit_dict, key = fav_fruit_dict.get)
-print(least_fav_fruit)
+print(f" Least most common favorite fruit is : {least_fav_fruit}")
 
 
 #Total number of unread messages for all users
@@ -113,5 +113,5 @@ def message_count(str):
 total_messages = 0
 for i in range(0, len(profiles)):
     total_messages = total_messages + message_count(profiles[i]['greeting'])
-print(f" total messages = {total_messages}")
+print(f" total unread messages = {total_messages}")
     

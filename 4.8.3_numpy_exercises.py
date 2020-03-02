b9587b463cda3a3f347eef7350e52153eb5b4c7e
Ravinder Singh <ravinder79@gmail.com>
2020-03-02 15:17:03 -0600

    more numpy exercises

--- 4.8.3_numpy_exercises.py
+++ 4.8.3_numpy_exercises.py
@@ -106,3 +106,160 @@ print(odds_in_a)
 
 evens_in_a = [i for i in a if i % 2 == 0]
 print(evens_in_a)
+
+
+## What about life in two dimensions? A list of lists is matrix, a table, a spreadsheet, a chessboard...
+## Setup 2: Consider what it would take to find the sum, min, max, average, sum, product, and list of squares for this list of two lists.
+b = [
+    [3, 4, 5],
+    [6, 7, 8]
+]
+
+
+sum_of_b = 0
+for i  in range(0, len(b)):
+    sum_of_b = (sum_of_b + sum(b[i]))
+
+print(sum_of_b)
+
+
+if min(b[0])< min(b[1]):
+    min_of_b = min(b[0])
+else:
+    min_of_b = min(b[1])
+
+print(min_of_b)
+
+
+if max(b[0])< max(b[1]):
+    max_of_b = max(b[1])
+else:
+    max_of_b = max(b[0])
+
+print(max_of_b)
+
+mean_of_b = sum_of_b/(len(b[0])+len(b[1]))
+print(mean_of_b)
+
+prod_of_b = 1
+for i in b:
+    for e in i:
+        prod_of_b = prod_of_b * e
+
+print(prod_of_b)
+
+squares_of_b = []
+for i in b:
+    for e in i:
+        squares_of_b.append(e ** 2)
+
+print(squares_of_b)
+
+# Exercise 1 - refactor the following to use numpy. Use sum_of_b as the variable. **Hint, you'll first need to make sure that the "b" variable is a numpy array**
+
+b = [
+    [3, 4, 5],
+    [6, 7, 8]
+]
+
+b = np.array(b)
+
+sum_of_b = b.sum()
+print(sum_of_b)
+
+# Exercise 2 - refactor the following to use numpy. 
+#min_of_b = min(b[0]) if min(b[0]) <= min(b[1]) else min(b[1])  
+
+min_of_b = b.min()
+print(min_of_b)
+# Exercise 3 - refactor the following maximum calculation to find the answer with numpy.
+#max_of_b = max(b[0]) if max(b[0]) >= max(b[1]) else max(b[1])
+max_of_b = b.max()
+print(max_of_b)
+
+# Exercise 4 - refactor the following using numpy to find the mean of b
+#mean_of_b = (sum(b[0]) + sum(b[1])) / (len(b[0]) + len(b[1]))
+mean_of_b = b.mean()
+print(mean_of_b)
+
+
+# Exercise 5 - refactor the following to use numpy for calculating the product of all numbers multiplied together.
+# product_of_b = 1
+# for row in b:
+#     for number in row:
+#         product_of_b *= number
+
+product_of_b = b.prod()
+print(prod_of_b)
+# Exercise 6 - refactor the following to use numpy to find the list of squares 
+# squares_of_b = []
+# for row in b:
+#     for number in row:
+#         squares_of_b.append(number**2)
+
+squares_of_b = np.square(b)
+print(squares_of_b)
+
+# Exercise 7 - refactor using numpy to determine the odds_in_b
+# odds_in_b = []
+# for row in b:
+#     for number in row:
+#         if(number % 2 != 0):
+#             odds_in_b.append(number)
+
+odds_in_b = b[b % 2 == 1]
+print(odds_in_b)
+
+# Exercise 8 - refactor the following to use numpy to filter only the even numbers
+# evens_in_b = []
+# for row in b:
+#     for number in row:
+#         if(number % 2 == 0):
+#             evens_in_b.append(number)
+
+evens_in_b = b[b % 2 == 0]
+print(evens_in_b)
+# Exercise 9 - print out the shape of the array b.
+
+print(b.shape)
+
+# Exercise 10 - transpose the array b.
+
+print(b.T)
+
+
+# Exercise 11 - reshape the array b to be a single list of 6 numbers. (1 x 6)
+
+b.reshape(1, 6)
+print(b.reshape(1, 6))
+
+
+# Exercise 12 - reshape the array b to be a list of 6 lists, each containing only 1 number (6 x 1)
+b.reshape(-1,6)
+print(b.reshape(,1))
+
+
+## Setup 3
+c = [
+    [1, 2, 3],
+    [4, 5, 6],
+    [7, 8, 9]
+]
+
+# HINT, you'll first need to make sure that the "c" variable is a numpy array prior to using numpy array methods.
+# Exercise 1 - Find the min, max, sum, and product of c.
+
+# Exercise 2 - Determine the standard deviation of c.
+
+# Exercise 3 - Determine the variance of c.
+
+# Exercise 4 - Print out the shape of the array c
+
+# Exercise 5 - Transpose c and print out transposed result.
+
+# Exercise 6 - Get the dot product of the array c with c. 
+
+# Exercise 7 - Write the code necessary to sum up the result of c times c transposed. Answer should be 261
+
+# Exercise 8 - Write the code necessary to determine the product of c times c transposed. Answer should be 131681894400.
+

a8c0e0aa73e61e1cebb7ad8ebd28f1d5da9cb76b
Ravinder Singh <ravinder79@gmail.com>
2020-03-01 20:14:15 -0600

    Pythong practice codewars

--- Python_practice-Codewars.py
+++ Python_practice-Codewars.py
@@ -73,3 +73,13 @@ def solution(num):
     else:
         return 0
     
+
+# Given a positive integer n written as abcd... (a, b, c, d... being digits) and a positive integer p
+
+# we want to find a positive integer k, if it exists, such as the sum of the digits of n taken to the successive powers of p is equal to k * n.
+# In other words:
+# Is there an integer k such as : (a ^ p + b ^ (p+1) + c ^(p+2) + d ^ (p+3) + ...) = n * k
+
+def dig_pow(n, p):
+    # your code
+    return -1

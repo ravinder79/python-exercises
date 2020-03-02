06f72d337febb83435b6f343fdbb78c49cfe2230
Ravinder Singh <ravinder79@gmail.com>
2020-03-02 14:02:06 -0600

    4.8.3 with more numpy exercises

--- 4.8.3_numpy_exercises.py
+++ 4.8.3_numpy_exercises.py
@@ -54,3 +54,55 @@ print(a)
 
 z_scores = a/stddev
 print(z_scores)
+
+## Setup 1
+a = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
+
+# Use python's built in functionality/operators to determine the following:
+# Exercise 1 - Make a variable called sum_of_a to hold the sum of all the numbers in above list
+# a = np.array(a)
+
+# sum_of_a = a.sum()
+# print(sum_of_a)
+
+sum_of_a = sum(a)
+print(sum_of_a)
+# Exercise 2 - Make a variable named min_of_a to hold the minimum of all the numbers in the above list
+
+# min_of_a = a.min()
+# print(min_of_a)
+min_of_a = min(a)
+print(min_of_a)
+
+# Exercise 3 - Make a variable named max_of_a to hold the max number of all the numbers in the above list
+max_of_a = max(a)
+print(max_of_a)
+
+# Exercise 4 - Make a variable named mean_of_a to hold the average of all the numbers in the above list
+
+mean_of_a = sum(a)/len(a)
+print(mean_of_a)
+
+# Exercise 5 - Make a variable named product_of_a to hold the product of multiplying all the numbers in the above list together
+
+prod = 1
+for i in range(0, len(a)):
+    prod = a[i] * prod
+
+product_of_a = prod
+print(product_of_a)
+
+# Exercise 6 - Make a variable named squares_of_a. It should hold each number in a squared like [1, 4, 9, 16, 25...]
+squares_of_a = [i*i for i in a]
+
+
+print(squares_of_a)
+
+# Exercise 7 - Make a variable named odds_in_a. It should hold only the odd numbers
+odds_in_a = [i for i in a if i % 2 ==1]
+print(odds_in_a)
+
+# Exercise 8 - Make a variable named evens_in_a. It should hold only the evens.
+
+evens_in_a = [i for i in a if i % 2 == 0]
+print(evens_in_a)
